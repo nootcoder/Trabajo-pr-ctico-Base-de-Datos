788f5f07dd2fd35f75c5f50b2bdfd6a81ce32e96
micaalonso17@gmail.com <micaalonso17@gmail.com>
2024-12-04 06:45:46 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +9,16 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_agregar_datos_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    result = anvil.server.call('add_to_database', 
+                               self.input_nombre.text,
+                              self.input_apellido.text,
+                              self.input_materia.text,
+                              self.input_nota.text)
+    if result:
+      self.result_1.visible = True
+      self.result_1.text = result.capitalize()
+      
+      
